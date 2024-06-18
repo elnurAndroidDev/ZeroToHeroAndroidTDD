da51830a725dfc000a8318c47a724fb5e3e9be74
Isaev Elnur <isayeveln@gmail.com>
2024-06-18 12:17:19 +0500

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,25 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            binding.actionButton.isEnabled = false
+            binding.progressBar.visibility = View.VISIBLE
+            binding.actionButton.postDelayed({
+                binding.titleTextView.visibility = View.VISIBLE
+                binding.actionButton.isEnabled = true
+                binding.progressBar.visibility = View.GONE
+            }, 2000)
+        }
     }
 }
