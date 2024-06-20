e35273cde48be3b07352770d70b66f81e8920d4b
Isaev Elnur <isayeveln@gmail.com>
2024-06-20 18:28:49 +0500

    fix uistate

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -13,7 +13,7 @@ interface UiState {
         override fun apply(button: Button, progressBar: ProgressBar, textView: TextView) {
             button.isEnabled = false
             progressBar.visibility = View.VISIBLE
-            textView.visibility = View.INVISIBLE
+            textView.visibility = View.GONE
         }
     }
 
