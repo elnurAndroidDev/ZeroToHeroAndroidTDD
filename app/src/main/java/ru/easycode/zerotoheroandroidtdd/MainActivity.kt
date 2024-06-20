472170e0fce668c451f35203a8633698be96c13c
Isaev Elnur <isayeveln@gmail.com>
2024-06-20 10:58:48 +0500

    solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,22 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel = MainViewModel(LiveDataWrapper.Base(), Repository.Base())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            viewModel.load()
+        }
+
+        viewModel.liveData().observe(this) { uiState ->
+            uiState.apply(binding.actionButton, binding.progressBar, binding.titleTextView)
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,27 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.view.View
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+
+interface UiState {
+
+    fun apply(button: Button, progressBar: ProgressBar, textView: TextView)
+
+    object ShowProgress : UiState {
+        override fun apply(button: Button, progressBar: ProgressBar, textView: TextView) {
+            button.isEnabled = false
+            progressBar.visibility = View.VISIBLE
+            textView.visibility = View.INVISIBLE
+        }
+    }
+
+    object ShowData : UiState {
+        override fun apply(button: Button, progressBar: ProgressBar, textView: TextView) {
+            button.isEnabled = true
+            progressBar.visibility = View.INVISIBLE
+            textView.visibility = View.VISIBLE
+        }
+    }
+}
